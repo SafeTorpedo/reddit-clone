5ffcc7b57a3cfcc48a97f112759712da2df76c04
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-05-02 21:59:23 +0530

    minor: final touches

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [nav, setNav] = useState(true);
@@ -9,7 +10,9 @@ const Navbar = () => {
 
     return (
         <div className="flex  justify-between items-center h-20 max-w-[1240px] mx-auto px-4 text-orange-600 ">
-            <h1 className="w-full text-3xl font-bold  ">My-Reddit</h1>
+            <Link to="/">
+                <h1 className="w-full text-3xl font-bold ">My-Reddit</h1>
+            </Link>
             <ul className="hidden md:flex">
                 <li className="p-4">
                     <a
@@ -35,9 +38,11 @@ const Navbar = () => {
                         : "fixed left-[-100%]"
                 }
             >
-                <h1 className="w-full text-3xl font-bold text-orange-600 m-8 p-6">
-                    My-Reddit
-                </h1>
+                <Link to="/">
+                    <h1 className="w-full text-3xl font-bold text-orange-600 m-8 p-6">
+                        My-Reddit
+                    </h1>
+                </Link>
                 <ul className="pt-12 uppercase p-4">
                     <li className="p-4">
                         <a
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,30 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center items-center text-orange-600 "></div>
+            <div className="flex justify-center items-center text-orange-600 p-10 text-center font-medium text-xl ">
+                Hi, I was unable to implement User auth for now due to time
+                constraints and lack of user auth experience (as I have mostly
+                worked on the frontend side of team projects)
+                <br />
+                <br />
+                As User Auth is not implemented, upvotes features is still
+                pending as without auth, a user will be able to upvote multiple
+                times.
+                <br />
+                <br />
+                Current features are create/delete Post, create/delete/update
+                Comment, create/delete/update nested Comments with basic styling
+                asthetics. The website is fully responsive.
+            </div>
+            <div className="flex justify-center items-center text-orange-600 p-10 text-center font-medium text-xl ">
+                <Link to="/posts">
+                    Click to see posts (Current user is Prath)
+                </Link>
+            </div>
         </>
     );
 };
